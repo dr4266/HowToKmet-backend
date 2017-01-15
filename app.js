76916b446167f9fb10ba830e244b4d9567041d39
David <david.rubin95@gmail.com>
2017-01-15 15:52:32 +0100

    Rollback to http/1.1, since heroku doesn't support SPDY... WTH??

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'prod')));
+app.enable('trust proxy');
 
 
 // Add headers before defining routes
